4d234c8fefb0e6d2762e8c52abecfdfeec3a55e5
Edilson <edilson.ebert@gmail.com>
2026-05-25 14:18:02 -0300

    Criada a condição de aluno destaque

--- aula.py
+++ aula.py
@@ -1,7 +1,10 @@
 nota = float(input("Digite a nota: "))
 
-if nota >= 6.0:
+if nota >= 6.0 and nota < 9.0:
     print("Aprovado")
+
+elif nota >= 9.0:
+    print("Aluno destaque")
 
 elif nota >= 4.0 and nota < 6.0:
     print("Recuperação")
